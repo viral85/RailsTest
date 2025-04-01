ce5ea9fc493d6451d75f5b23351916cd0006eb38
Viral Sonawala <viral85@gmail.com>
2025-04-01 10:29:01 +0530

    feat: Added nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "invoices#new"
+  resources :invoices
+  resources :checks
+  resources :companies
 end
